ec82f484e1cce9e7f22c7759ba4586b7eba65a54
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-04 15:03:13 +0100

    Revert "fix(puppeteer): ci environment"
    This reverts commit d1963a62546e526018dab55ad4e61efce159fac3.

--- marp.config.mjs
+++ marp.config.mjs
@@ -7,18 +7,11 @@ const config = {
     if (canonicalUrl) return `${canonicalUrl}/og-image.jpg`
     if (process.env.VERCEL_URL)
       return `https://${process.env.VERCEL_URL}/og-image.jpg`
-
+  
     return undefined
   })(),
   themeSet: 'themes',
   url: canonicalUrl,
-  puppeteerLaunchArgs: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-dev-shm-usage',
-    '--disable-accelerated-2d-canvas',
-    '--disable-gpu',
-  ],
 }
 
 export default config
