d1963a62546e526018dab55ad4e61efce159fac3
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-04 14:56:32 +0100

    fix(puppeteer): ci environment
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- marp.config.mjs
+++ marp.config.mjs
@@ -7,11 +7,18 @@ const config = {
     if (canonicalUrl) return `${canonicalUrl}/og-image.jpg`
     if (process.env.VERCEL_URL)
       return `https://${process.env.VERCEL_URL}/og-image.jpg`
-  
+
     return undefined
   })(),
   themeSet: 'themes',
   url: canonicalUrl,
+  puppeteerLaunchArgs: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--disable-gpu',
+  ],
 }
 
 export default config
